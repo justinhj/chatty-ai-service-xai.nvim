79af6b2414269eebb0967b35fc7c198e8ccec053
Justin Heyes-Jones <justinhj@gmail.com>
2024-10-27 20:59:55 -0700

    get openai working

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Chatty-AI Ollama service
+# Chatty-AI OpenAI service
 
-This is a plugin that enables support for completions by Ollama's built in server, and this is only useful used in conjunction with [chatty-ai](https://github.com/justinhj/chatty-ai.nvim).
+This is a plugin that enables support for completions by OpenAI's ChatGPT models, and this is only useful used in conjunction with [chatty-ai](https://github.com/justinhj/chatty-ai.nvim).
 
 ## Usage
 
-After installing the chatty-ai system, you should also install [Ollama](https://github.com/ollama) so that you can run local models.
-
-Follow the instructions to install the models you would like to use with chatty-ai as completion services. Once installed you can cofigure an Ollama service and register it with chatty-ai so that it can be used.
+After installing the chatty-ai system, you should also register for an OpenAI account and obtain an API key. Once you have the key please set it as an environment variable named `OPENAI_API_KEY` (configurable), so that it can be accessed by the plugin when making completions.
 
 In [./sampleconfig/config.lua](./sampleconfig/config.lua) you can find simple instructions to do this.
-
-
-
--- init.lua
+++ init.lua
@@ -1,5 +1,22 @@
 local chatty = require('chatty-ai')
 
+-- https://platform.openai.com/docs/api-reference/chat
+
+-- > curl https://api.openai.com/v1/chat/completions \
+--   -H "Content-Type: application/json" \
+--   -H "Authorization: Bearer $OPENAI_API_KEY" \
+--   -d '{
+--      "model": "gpt-4o",
+--      "messages": [{"role": "user", "content": "Say this is a test!"}],
+--      "temperature": 0.7
+--    }'
+
+-- chunk response
+-- {"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-4o-mini", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{"role":"assistant","content":""},"logprobs":null,"finish_reason":null}]}
+
+-- {"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-4o-mini", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"stop"}]}
+
+
 ---@class CompletionServiceConfig
 ---@field public name string
 ---@field public stream_error_cb function
@@ -9,31 +26,17 @@ local chatty = require('chatty-ai')
 ---@field public stream_cb function
 ---@field public configure_call function
 
--- > curl http://localhost:11434/api/chat -d '{
---   "model": "qwen2.5:3b", "stream": false,
---   "messages": [
---     {
---       "role": "user",
---       "content": "what are some popular declarative programming languages? Please limit to 5"
---     }
---   ]
--- }'
-
----@class OllamaConfig
----@field scheme string?
----@field host string?
----@field port number?
+---@class OpenAIConfig
+---@field api_key_name string?
 ---@field model string?
 
----@type OllamaConfig
+---@type OpenAIConfig
 local default_config = {
-  scheme = 'http',
-  host = 'localhost',
-  port = 11434,
-  model = 'qwen2.5:3b',
+  api_key_name = 'OPENAI_API_KEY',
+  model = 'gpt-4o',
 }
 
-local CHAT_PATH = '/api/chat'
+local OPENAI_URL = 'https://api.openai.com/v1/chat/completions'
 
 local source = {}
 
@@ -49,50 +52,80 @@ end
 -- return url, headers, body
 source.configure_call = function(self, user_prompt, completion_config, is_stream)
   local config = self.config
-  local url = config.scheme .. '://' .. config.host .. ':' .. config.port .. CHAT_PATH
-  local headers = {}
+  local url = OPENAI_URL
+  local api_key = os.getenv(config.api_key_name)
+  if not api_key then
+    error('OpenAI api key \'' .. config.api_key_name .. '\' not found in environment.')
+  end
+  local headers = {
+      ['Authorization'] = 'Bearer ' .. api_key,
+      ['Content-Type'] = 'application/json',
+    }
+
   local body = {
     model = config.model,
-    stream = is_stream,
     messages = {
+      { role = 'system',
+        content = completion_config.system
+      },
       { role = 'user',
         content = user_prompt,
       },
     }
   }
+
+  if is_stream then
+    body['stream'] = true
+    body['stream_options'] = { include_usage = is_stream }
+  end
+
+  vim.print(vim.inspect(headers) .. vim.inspect(body))
   return url, headers, body
 end
 
 source.complete_cb = function(response)
-  local parsed_response = vim.fn.json_decode(response.body)
-  local input_tokens = parsed_response.prompt_eval_count
-  local output_tokens = parsed_response.eval_count
-  local content = parsed_response.message.content
+  local status, parsed_response = pcall(vim.fn.json_decode, response.body)
+  if status then
+    local input_tokens = parsed_response.usage.prompt_tokens or 0
+    local output_tokens = parsed_response.usage.completion_tokens or 0
+    local content = ''
+    local choice = parsed_response.choices[1]
+    content = choice.message.content
 
-  return {
-    content = content,
-    input_tokens = input_tokens,
-    output_tokens = output_tokens,
-  }
+    return {
+      content = content,
+      input_tokens = input_tokens,
+      output_tokens = output_tokens,
+    }
+  else
+    error(parsed_response)
+  end
 end
 
-source.stream_cb = function(chunk)
-  local status, data = pcall(vim.json.decode, chunk)
-  if status then
+source.stream_cb = function(raw_chunk)
+  local chunk = raw_chunk:gsub("^data: ", "")
+  if #chunk == 0 or chunk == '[DONE]' then
+    return ''
+  else
+    local status, data = pcall(vim.json.decode, chunk)
     local content = ''
-    if data.message and data.message.content then
-      content = data.message.content
+    if status then
+      if #data.choices == 0 or data.choices[1].finish_reason == 'stop' then
+        return ''
+      elseif data.choices and data.choices[1].delta then
+        content = data.choices[1].delta.content
+      end
+      return content
+    else
+      error(data)
     end
     return content
-  else
-    error(data)
   end
 end
 
 source.stream_complete_cb = function(response)
   local body = response.body
   local lines = {}
-  local text = ""
 
   for line in body:gmatch("[^\r\n]+") do
     table.insert(lines, line)
@@ -100,21 +133,28 @@ source.stream_complete_cb = function(response)
 
   local input_tokens = 0
   local output_tokens = 0
+  local content = ''
 
-  for _, line in ipairs(lines) do
-    local data = vim.fn.json_decode(line)
-    if data.reponse then
-      text = text .. data.response
-    end
-    if data.prompt_eval_count then
-      input_tokens = data.prompt_eval_count
-    end
-    if data.eval_count then
-      output_tokens = data.eval_count
+  for _, raw_chunk in ipairs(lines) do
+    local chunk = raw_chunk:gsub("^data: ", "")
+    if #chunk == 0 or chunk == '[DONE]' then
+      return content, input_tokens, output_tokens
+    else
+      local status, data = pcall(vim.json.decode, chunk)
+      local content = ''
+      if status then
+        if #data.choices == 0 or data.choices[1].finish_reason == 'stop' then
+        elseif data.choices and data.choices[1].delta then
+          content = content .. data.choices[1].delta.content
+        elseif data.usage and type(data.usage) == 'table' then
+          input_tokens = data.usage.prompt_tokens
+          output_tokens = data.usage.completion_tokens
+        end
+      else
+        error(data)
+      end
     end
   end
-
-  return text, input_tokens, output_tokens
 end
 
 return {
